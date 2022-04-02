24f71ed349b44b1f9294871cd2c5da0a106072c7
unknown <whitebeard086@gmail.com>
2022-04-02 11:42:49 +0100

    hooked up github api

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Footer } from "./layout/Footer";
 export { default as Navbar } from "./layout/Navbar";
+export { default as Spinner } from "./layout/Spinner";
+export { default as UserItem } from "./users/UserItem";
+export { default as UserResults } from "./users/UserResults";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { UserResults } from "../components"
+
 const Home = () => {
   return (
-    <div>
-        <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+        <UserResults />
+    </>
   )
 }
 export default Home
