2cd6904e2638cc144929b73811667dff659ab053
unknown <whitebeard086@gmail.com>
2022-04-04 10:25:01 +0100

    bug fix

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -41,6 +41,8 @@ const User = () => {
 
   if (loading) return <Spinner />;
 
+  const websiteUrl = blog?.startsWith("http") ? blog : "https://" + blog;
+
   return (
     <>
       <div className="w-full mx-auto lg:w-10/12">
@@ -49,11 +51,12 @@ const User = () => {
             Back To Search
           </Link>
         </div>
-        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'">
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
           <div className="custom-card-image mb-6 md:mb-0">
-            <div className="rounded-lg shadow-xl card image-full mr-4">
+            <div className="rounded-lg shadow-xl card image-full">
               <figure>
-                <img src={avatar_url} alt="User" />
+                <img src={avatar_url} alt="" />
               </figure>
               <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
@@ -61,6 +64,7 @@ const User = () => {
               </div>
             </div>
           </div>
+
           <div className="col-span-2">
             <div className="mb-6">
               <h1 className="text-3xl card-title">
@@ -70,15 +74,12 @@ const User = () => {
               </h1>
               <p>{bio}</p>
               <div className="mt-4 card-actions">
-                <a
-                  href={html_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-outline">
-                  Visit GitHub Profile
+                <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-outline">
+                  Visit Github Profile
                 </a>
               </div>
             </div>
+
             <div className="w-full rounded-lg shadow-md bg-base-100 stats">
               {location && (
                 <div className="stat">
@@ -90,8 +91,8 @@ const User = () => {
                 <div className="stat">
                   <div className="stat-title text-md">Website</div>
                   <div className="text-lg stat-value">
-                    <a href={`https://${blog}`} target="_blank" rel="noopener noreferrer">
-                      {blog}
+                    <a href={websiteUrl} target="_blank" rel="noreferrer">
+                      {websiteUrl}
                     </a>
                   </div>
                 </div>
@@ -103,7 +104,7 @@ const User = () => {
                     <a
                       href={`https://twitter.com/${twitter_username}`}
                       target="_blank"
-                      rel="noopener noreferrer">
+                      rel="noreferrer">
                       {twitter_username}
                     </a>
                   </div>
@@ -112,36 +113,43 @@ const User = () => {
             </div>
           </div>
         </div>
+
         <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUsers className="text-3xl md:text-5xl" />
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">{followers}</div>
             </div>
-            <div className="stat-title pr-5">Followers</div>
-            <div className="stat-value pr-5 text-3xl">{followers}</div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaUserFriends className="text-3xl md:text-5xl" />
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">{following}</div>
             </div>
-            <div className="stat-title pr-5">Following</div>
-            <div className="stat-value pr-5 text-3xl">{following}</div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaCodepen className="text-3xl md:text-5xl" />
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">{public_repos}</div>
             </div>
-            <div className="stat-title pr-5">Public Repos</div>
-            <div className="stat-value pr-5 text-3xl">{public_repos}</div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <FaStore className="text-3xl md:text-5xl" />
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaStore className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Gists</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">{public_gists}</div>
             </div>
-            <div className="stat-title pr-5">Public Gists</div>
-            <div className="stat-value pr-5 text-3xl">{public_gists}</div>
           </div>
         </div>
+
         <RepoList repos={repos} />
       </div>
     </>
